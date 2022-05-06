e0cf9ac903aa0814f6bc77f696bfdee90765dae5
Suporte <Suporte@DESKTOP-PIOR8K9>
2022-05-06 19:39:19 -0300

    Add project files.

---
+++ Entra21.ExercicicsWhile/ExercicioWhile01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entra21.ExerciciosWhile
+{
+    internal class ExercicioWhile01
+    {
+        public void Executar()
+        {
+            double preco = 0;
+            string nome = "";
+            int indice = 0;
+
+            while (indice = 13)
+        }
+    }
+}
---
+++ Entra21.ExercicicsWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using Entra21.ExerciciosWhile;
+
+Console.WriteLine(@"---------------MENU------------
+1-Exercicio 01
+2-Exercicio 02
+3-Exercicio 03");
+
+Console.Write("Digite a opção desejada");
+int opcaoDesejada = Convert.ToInt32(Console.ReadLine());
+
+if (opcaoDesejada == 1)
+{
+    ExercicioWhile01 Atividade = new ExercicioWhile01();
+    Atividade.Executar();
+}
+else if (opcaoDesejada == 2)
+{
+    ExercicioWhile02 Atividade = new ExercicioWhile02();
+    Atividade.Executar();
+}
+else if
