958babbfa6678b21274200519e5ea9c8ac0c1885
JulianaForbici <julianaforbicii@gmail.com>
2022-05-26 22:10:11 -0300

    exercicios atualizacao

---
+++ ExerciciosOrientacaoObjeto/Exercicio01/ExemploLivro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosOrientacaoObjeto.Exercicio01
+{
+    public class ExemploLivro
+    {
+        public void Executar()
+        {
+            Livro livro = new Livro();
+           
+            livro.Titulo = "Felicidade";
+            livro.Autor = "alan";
+            livro.CodigoISBN = "40028922";
+            livro.Idioma = "Portugues";
+            
+
+
+        }
+       
+
+    }
+}
---
+++ ExerciciosOrientacaoObjeto/Exercicio01/Livro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosOrientacaoObjeto.Exercicio01
+{
+    public class Livro
+    {
+        public string Titulo;
+        public string Autor;
+        public string CodigoISBN;
+        public string Idioma;
+        public DateTime DataLancamento;
+        public int QuantidadePaginas;
+        public int PaginasLidas;
+        public int QuantidadeReleitura;
+    }
+
+} 
