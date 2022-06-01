d8d06131eede7b0b3bcbebd0c161e15374ad1d26
alan.fernandes <alan.fernandes@ACESSO01>
2022-06-01 19:46:09 -0300

    atualizacao nova

---
+++ ExerciciosOrientacaoObjeto/Exercicio03/Conversor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosOrientacaoObjeto.Exercicio03
+{
+    public class ConversorAscii
+    {
+        /*Criar uma classe chamada ConversorAscii
+        Caracter
+
+        Criar métodos:
+        Obter código ascii do caracter.Levar em consideração que pode ser em caixa
+        alta ou caixa baixa.
+        Obs.: É obrigatório fazer os ifs para descobrir o código ascii*/
+
+        string caracter = "";
+
+        
+
+
+    }
+}
