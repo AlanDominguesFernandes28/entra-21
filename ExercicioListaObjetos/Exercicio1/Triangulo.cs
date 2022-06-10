b660fe10fcfa098fb4eebc84b39e862008e7ef72
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-06-09 22:11:07 -0300

    Exercicio lista

---
+++ ExercicioListaObjetos/Exercicio1/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioListaObjetos.Exercicio1
+{
+    //+lado 1
+    //+lado 2
+    //+lado 3
+    //+codigo
+    //-----------------------
+    //ValidarTriangulo():bool
+    internal class Triangulo
+    {
+        public int lado1;
+        public int lado2;
+        public int lado3;
+        public int codigo;
+
+    }
+    public int ValidarTriangulo()
+    {
+        if(lado1 > lado2 && lado3)
+        {
+            return true;
+        }
+        else
+        {
+
+        }
+
+        
+
+    }
+}
---
+++ ExercicioListaObjetos/Exercicio1/TrianguloServico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioListaObjetos.Exercicio1
+{
+    //-Triangulos:List<Triangulo>
+    //-CodigoAtual
+    //---------------------------------------
+    //+Adicionar(lado1,lado2,lado3):bool
+    //+Editar(codigo,lado1,lado2,lado3):bool
+    //+Apagar(codigo):bool
+    //+ObterTodos():list<Triangulo>
+    //+ObterPorCodigo(codigo):Triangulo
+    internal class TrianguloServico
+    {
+    }
+}
