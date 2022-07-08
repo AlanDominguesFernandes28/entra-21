99b09ae08425865fafd2734c6688a55bf0e4c5b5
AlanDominguesFernandes28 <alan786k@gmail.com>
2022-07-07 22:02:53 -0300

    Atualizacao 2

--- ExerciciosSQL/ExercicioSQL2.sql
+++ ExerciciosSQL/ExercicioSQL2.sql
@@ -12,47 +12,47 @@ SELECT estado,cidade,bairro,cep,logradouro,numero,complemento FROM enderecos;
 
 INSERT INTO enderecos
 	(estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES ('AC ','Rio Branco','Ayrton Senna ','69.911-866 ','Estrada Deputado José Rui da Silveira Lino','282 ','Casa');
+	VALUES ('AC','Rio Branco','Ayrton Senna','69.911-866','Estrada Deputado José Rui da Silveira Lino','282','Casa');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES ('SC','Biguaçu ','Fundos ','88.161-400 ','Rua Julio Teodoro Martins','995','');
+	VALUES ('SC','Biguaçu','Fundos','88.161-400','Rua Julio Teodoro Martins','995','');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES ('MG ','Santa Luzia','Padre Miguel ','33.082-050','Rua Buenos Aires ','371 ','Apartamento');
+	VALUES ('MG','Santa Luzia','Padre Miguel','33.082-050','Rua Buenos Aires','371','Apartamento');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('BA ','','São Tomé de Paripe','40.800-361 ','Travessa Luís Hage','685','');
+	VALUES('BA','','São Tomé de Paripe','40.800-361','Travessa Luís Hage','685','');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('MG ','Ipatinga ','Vila Celeste ','','Rua Antônio Boaventura Batista','645','');
+	VALUES('MG','Ipatinga','Vila Celeste','','Rua Antônio Boaventura Batista','645','');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('RS ','Passo Fundo ','Nenê Graeff ','99.030-250','','154','');
+	VALUES('RS','Passo Fundo','Nenê Graeff','99.030-250','','154','');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('AM ','Manaus','Petrópolis','69.079-300 ','Rua Coronel Ferreira Sobrinho','264','Fundos');
+	VALUES('AM','Manaus','Petrópolis','69.079-300','Rua Coronel Ferreira Sobrinho','264','Fundos');
 
 INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('TO ','Gurupi','Gurupi','77.402-130 ','Rua Adelmo Aires Negri ','794','');
+	VALUES('TO','Gurupi','Gurupi','77.402-130','Rua Adelmo Aires Negri','794','');
 
 	INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('AC','','Preventório','','Preventório','212 ','Bloco B');
+	VALUES('AC','','Preventório','','Preventório','212','Bloco B');
 
 	INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('AP ','Santana','Comercial ','68.925-073 ','Rua Calçoene ','648','');
+	VALUES('AP','Santana','Comercial','68.925-073','Rua Calçoene','648','');
 
 	INSERT INTO enderecos
     (estado,cidade,bairro,cep,logradouro,numero,complemento)
-	VALUES('PB ','Cabedelo','Camalaú ','58.103-052 ','Rua Siqueira Campos','249','');
+	VALUES('PB','Cabedelo','Camalaú','58.103-052','Rua Siqueira Campos','249','');
 
 --atualizar a lista--
 UPDATE enderecos SET cep = '69.911-866', logradouro ='Estrada Deputado José Rui da Silveira Lino' WHERE numero = '282';
